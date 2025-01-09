6eeb11999de909ad990bae44c030932969ac5b54
“[Sumit <sagnihotri9710@gmail.com>
2025-01-09 14:42:09 +0530

    Variable

---
+++ VARIABLES/Q3 Rules for Variables.py
@@ -0,0 +1,6 @@
+# Guys, we have to follow some rules for naming Variables in Python. Here they are:
+
+print("1:-> Variable names must start with a letter (a-z, A-Z) or an underscore (_).")
+print("2:-> The rest of the name can contain letters, numbers, and underscores.")
+print("3:-> Variable names are case-sensitive.")
+print("4:-> Variable names cannot be Python reserved keywords (e.g., if, else, for, while).")
