77a9ff77c4daaf9d466ba7b37fb149f3c060ef5b
“[Sumit <sagnihotri9710@gmail.com>
2025-01-09 14:59:52 +0530

    Variable

--- Q5 Swapping 2 Values.py
+++ Q5 Swapping 2 Values.py
@@ -1,6 +1,6 @@
 # Write a Python program to swap the values of two variables.
 a=10
 b=20
-a,b=b,a
-print("The value of 'a' has been swapped now and it's",a)
-print("The value of 'b' has been swapped now and it's",b)
+a,b=b,a # Swapping the values of a and b
+print("The value of \"a\" has been swapped now and it's",a) # Output: 20
+print("The value of \"b\" has been swapped now and it's",b) # Output: 10
