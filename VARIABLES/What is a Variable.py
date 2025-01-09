2564b93f3f75ce56aab9985403fce6f7aac4312b
“[Sumit <sagnihotri9710@gmail.com>
2025-01-09 14:35:54 +0530

    Initial commit

---
+++ VARIABLES/What is a Variable.py
@@ -0,0 +1,3 @@
+# 1. What is a Variable in Python?
+
+print("Answer: A Variable is a name used to store a value in memory.\n It acts as a reference to the data stored in the computer’s memory,\n allowing it to be used and modified during program execution.")
