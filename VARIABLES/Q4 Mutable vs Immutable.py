c85567b7b8cc54e9feb0d0df3807623308231da8
“[Sumit <sagnihotri9710@gmail.com>
2025-01-09 18:22:17 +0530

    Variable

--- VARIABLES/Q4 Mutable vs Immutable.py
+++ VARIABLES/Q4 Mutable vs Immutable.py
@@ -1,6 +1,5 @@
 # 4. What is the difference between mutable and immutable types in Python?
 
-
 print()
 print("* Mutable types can be changed after they are created (e.g., lists, dictionaries).")
 print()
