566427a362d44acfd55ffc0febdbe0a22ce176ee
“[Sumit <sagnihotri9710@gmail.com>
2025-01-09 18:27:37 +0530

    Variable

--- VARIABLES/Q6 Global Keyword.py
+++ VARIABLES/Q6 Global Keyword.py
@@ -1,10 +1,3 @@
 # 6. What is the global keyword in Python?
 
 print("The global keyword allows a function to modify a variable,\n that is defined outside of its scope (i.e., in the global scope).")
-
-# Example:
-x = 10 # Global variable
-def myfunc():
-  global x
-  x = 5 # Local variable
-myfunc()
