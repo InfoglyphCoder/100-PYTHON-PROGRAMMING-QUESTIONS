d015515c087dd207f3a6c81497ceef3e79bab7cc
“[Sumit <sagnihotri9710@gmail.com>
2025-01-10 15:44:43 +0530

    Variable

---
+++ VARIABLES/Q13 Taking User's Input.py
@@ -0,0 +1,5 @@
+# Write a Python program to store the user’s name and age using variables and print them.
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Name: {name}, Age: {age}")
