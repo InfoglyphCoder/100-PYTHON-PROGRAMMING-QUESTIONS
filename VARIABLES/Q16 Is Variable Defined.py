b3374cb40eacd79d55c29852dc0c7abbcde8100b
“[Sumit <sagnihotri9710@gmail.com>
2025-01-10 16:07:01 +0530

    Variable

---
+++ VARIABLES/Q16 Is Variable Defined.py
@@ -0,0 +1,8 @@
+# How do you check if a variable is defined in Python?
+print("You can use try-except to check if a variable is defined.")
+
+# Example:
+try:
+    print(x)
+except NameError:
+    print("Variable not defined")
