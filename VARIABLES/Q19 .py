70ab3a7ca0c01a2339c8e29fe8a6512ea3f4ac45
“[Sumit <sagnihotri9710@gmail.com>
2025-01-10 16:23:58 +0530

    Variable

---
+++ VARIABLES/Q19 .py
@@ -0,0 +1,2 @@
+# How does Python handle object references with variables?
+print("Python uses references for objects, meaning variables hold references to objects in memory,\n not the objects themselves.")
