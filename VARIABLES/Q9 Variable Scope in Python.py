b2da457d1cc8b21d0ceaaa32f8992b1ed33cfc63
“[Sumit <sagnihotri9710@gmail.com>
2025-01-10 15:10:43 +0530

    Variable

---
+++ VARIABLES/Q9 Variable Scope in Python.py
@@ -0,0 +1,10 @@
+
+
+x = 10  # Global variable
+
+def function():
+    y = 20  # Local variable
+    print(x, y)
+
+function()
+# print(y)  # This will cause an error because y is local to function()
