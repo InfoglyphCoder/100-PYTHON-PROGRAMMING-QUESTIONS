f9bc615f4c98c17938e9d0f820532be6e61fd9d7
“[Sumit <sagnihotri9710@gmail.com>
2025-01-10 16:01:47 +0530

    Variable

---
+++ VARIABLES/Q15 .py
@@ -0,0 +1,4 @@
+# What is a constant variable in Python? How can you create one?
+print("Constant Variables are typically represented by uppercase names by convention.")
+
+PI = 3.14  # Constant variable
