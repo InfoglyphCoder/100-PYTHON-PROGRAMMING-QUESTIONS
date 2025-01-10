98a3c2740221844f97c8bdc225c74d5d1a5b9796
“[Sumit <sagnihotri9710@gmail.com>
2025-01-10 16:09:26 +0530

    Variable

---
+++ VARIABLES/Q17 Variable Scroping.py
@@ -0,0 +1,2 @@
+# What is Variable Scoping in Python and how does it work?
+print("Variable Scoping refers to where a variable is accessible within the program./n Variables declared inside a function are local to that function, while variables declared outside are global.")
