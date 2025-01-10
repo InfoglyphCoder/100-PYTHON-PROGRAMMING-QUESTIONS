f17e063395cd9165fa4366049f2e05010691ad8f
“[Sumit <sagnihotri9710@gmail.com>
2025-01-10 15:35:22 +0530

    Variable

---
+++ VARIABLES/Q11 Handling Memory Location.py
@@ -0,0 +1,3 @@
+# How does Python handle variable memory allocation?
+
+print("Python uses a reference counting mechanism and a garbage collector to handle memory allocation and deallocation.\n Each object in Python is associated with a reference count.")
---
+++ VARIABLES/Q12 Same Variable with new values.py
@@ -0,0 +1,3 @@
+# What happens if you assign the same variable a new value in Python?
+
+print("The old value is discarded and the variable now references the new value.")
